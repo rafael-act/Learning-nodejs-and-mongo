acb5d59886a798a88cdf67f688a0c028a4eff651
rafael-act <rafael.actt@gmail.com>
2026-08-24 20:48:36 -0300

    feature: (17) - including backend logic - part 3

--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -3,7 +3,7 @@ function NoticiasDAO(connection) {
 }
 
 NoticiasDAO.prototype.getNoticias = function (callback) {
-  this._connection.query("SELECT * FROM noticias", callback);
+  this._connection.query("SELECT * FROM noticias ORDER BY data_criacao DESC", callback);
 };
 
 NoticiasDAO.prototype.getNoticia = function (callback) {
